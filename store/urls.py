926c383500066d36ba227be15d64eea0936fe12c
melaku05 <melakushiferaw05@gmail.com>
2022-09-10 14:31:52 +0300

    Add router views set for store

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter #SimpleRouter
 
 from . import views
 
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),
-    path('collections/', views.CollectionList.as_view()), 
-    path('collections/<int:pk>/', views.CollectionDetail.as_view() , name='collection-detail'),
-]
+router = DefaultRouter()
+router.register('products', views.ProductViewListSet)
+router.register('collections', views.CollectionViewListSet)
+urlpatterns = router.urls
